e5fcbadc8a6c8668b1321d02f44bfafe5529ab92
DanielSDVG <danielsdvg@gmail.com>
2021-11-18 13:38:17 +0100

    primer commit

---
+++ app/src/main/java/com/example/hysi/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.hysi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUsuario;
+    private EditText etContrasenia;
+    private Context appCtxt;
+    private Toast error;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        enlazarControles();
+    }
+
+    private void enlazarControles() {
+
+        etUsuario = findViewById(R.id.etUsuario);
+        etContrasenia = findViewById(R.id.etContrasenia);
+
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(v -> validarLogin());
+
+        Button btnRegistrate = findViewById(R.id.btnRegistrate);
+        btnRegistrate.setOnClickListener(v -> irARegistro());
+
+        appCtxt = getApplicationContext();
+        error = Toast.makeText(appCtxt, R.string.login_error, Toast.LENGTH_SHORT);
+
+    }
+
+    private void validarLogin() {
+        String usuario = etUsuario.getText().toString();
+        String contrasenia = etContrasenia.getText().toString();
+        if (false) {
+            // hacer el login
+        } else {
+            error.show();
+        }
+    }
+
+    private void irARegistro() {
+
+    }
+
+}
