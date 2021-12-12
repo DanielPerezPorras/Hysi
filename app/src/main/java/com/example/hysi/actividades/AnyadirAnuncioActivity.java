5b77db97a6ae7c035f62b8f79e53cfe66f8229b9
andrear99 <andrea99ro@uma.es>
2021-12-12 13:50:51 +0100

    update parte de andrea

---
+++ app/src/main/java/com/example/hysi/actividades/AnyadirAnuncioActivity.java
@@ -0,0 +1,24 @@
+package com.example.hysi.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hysi.R;
+import com.example.hysi.modelo.SingletonMap;
+import com.example.hysi.modelo.Usuario;
+
+public class AnyadirAnuncioActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anyadir_anuncio);
+
+    }
+}
--- app/src/main/java/com/example/hysi/actividades/MainActivity.java
+++ app/src/main/java/com/example/hysi/actividades/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.hysi.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hysi.R;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void ejecutarAnyadirAnuncio(View view) {
+        Intent i = new Intent(this, AnyadirAnuncioActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
